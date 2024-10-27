32549d9f7d73027d7b45ed994e728c2250499fc2
manhleodz <doanducmanh100@gmail.com>
2024-10-27 23:45:25 +0700

    aaaaa

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,12 @@ module.exports = (app) => {
 
     // redirect http traffic to https traffic
     app.use('*', (req, res, next) => {
-        if (!req.socket.encrypted) {
-            console.log('unsecure connection: redirecting..')
-            res.redirect('https://' + req.headers.host + req.path)
-        } else {
-            next()
-        }
+        // if (!req.socket.encrypted) {
+        //     console.log('unsecure connection: redirecting..')
+        //     res.redirect('https://' + req.headers.host + req.path)
+        // } else {
+        //     next()
+        // }
     })
 
     app.use(express.static(path.join(__dirname, '..', 'public')))
